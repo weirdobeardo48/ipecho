45251a20d36e38ebfdda6e8bca7d57895f007a2a
Nguyen Xuan Truong <cutruong.tn92@gmail.com>
2020-03-14 08:28:00 +0700

    Fuck, still typo

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.headers);
-  let ip = req.headers['x-forwarded-for'] == undefined ? req.connection.remoteAddress : req.headers['x-fowarded-for'];
+  let ip = req.headers['x-forwarded-for'] == undefined ? req.connection.remoteAddress : req.headers['x-forwarded-for'];
   res.status(200);
   res.send(ip);
 });
