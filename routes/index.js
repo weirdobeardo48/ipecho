b5bdd126f5e014a8dd992af49eeee18d5c8363fb
TruongNX25 <truongnx25@viettel.com.vn>
2020-05-15 14:21:25 +0700

    update /json endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log(req.headers);
   let ip = req.headers['x-forwarded-for'] == undefined ? req.connection.remoteAddress : req.headers['x-forwarded-for'];
   res.status(200);
@@ -10,3 +10,21 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.get('/json', function (req, res, next) {
+  try {
+    // console.log(req);
+    res.status(200);
+    let ip = req.headers['x-forwarded-for'] == undefined ? req.connection.remoteAddress : req.headers['x-forwarded-for'];
+    result = {};
+    result['IP'] = ip;
+    result['User-Agent'] = req.headers['user-agent'];
+    res.json(result);
+  } catch (e) {
+    console.log(e);
+    res.status(502);
+    res.json({
+      'status': 'error'
+    });
+  }
+});
